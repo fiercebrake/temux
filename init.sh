3abc9709dc0bac24d790ed157ae79b743c467824
David Valiente <fierce.brake@gmail.com>
2025-09-20 11:11:46 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,6 @@ pkg update -y && pkg upgrade -y
 pkg install -y python python-pip mlocate bash-completion pkgfile rsync git wget less gzip tar zlib openssh screen gnupg cronie
 
 touch ~/.ssh/id_rsa
+
+mv /data/data/com.termux/files/usr/bin/vi /data/data/com.termux/files/usr/bin/vi-bak
+ln -sf /data/data/com.termux/files/usr/bin/vim /data/data/com.termux/files/usr/bin/vi
