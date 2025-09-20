c7f07120b2e4e3d9996787bec144113d220c7bf6
David Valiente <fierce.brake@gmail.com>
2025-09-20 11:20:18 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,3 +8,17 @@ touch ~/.ssh/id_rsa
 
 mv /data/data/com.termux/files/usr/bin/vi /data/data/com.termux/files/usr/bin/vi-bak
 ln -sf /data/data/com.termux/files/usr/bin/vim /data/data/com.termux/files/usr/bin/vi
+
+cat << EOF > ~/.ssh/config
+Host tekne
+  HostName tekne.sv
+  Port 45100
+  User devops
+  IdentityFile ~/.ssh/id_rsa
+
+Host themis
+  HostName themis.tekne.sv
+  Port 45100
+  User dvaliente
+  IdentityFile ~/.ssh/id_rsa
+EOF
